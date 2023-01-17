157e682b7adac6702ea88fe55117a648a50de67c
Jonathan GUILLOT <jonathan@joggee.fr>
2023-01-17 10:16:12 +0100

    Add Screenshot button on Youtube Shorts (#30)
    * Add Screenshot button on Youtube Shorts
    
    * Directly use created link element to download screenshot
    
    Using querySelector() is not necessary here as the code creates
    the element first.
    
    * Add missing variable declaration in getFileName()
    
    * Redefine logger() when logging enabled
    
    It removes loggingEnabled global variable
    and avoids testing it each time a log has to be displayed.
    
    * Add hour in timestamp / filename
    
    By the way, use hyphen (-) instead of colon (:) to separate hours, minutes
    and seconds. It allows to be more coherent through different OS.
    
    * Fatorize addButtonOnRegularPlayer() and addButtonOnShortsPlayer()

--- content_script.js
+++ content_script.js
@@ -1,22 +1,37 @@
-var loggingEnabled = false;
+// Logger is disabled by default
+let logger = (message) => {
+  // Nothing
+};
+
 var copyToClipboardEnabled = false;
 
 // Image format
 var imageFormat = "image/jpeg";
 var imageFormatExtension = "jpeg";
 
+// Shorts container tag and active attribute
+const shortsContainerTag = "ytd-reel-video-renderer";
+const shortsContainerTagName = shortsContainerTag.toUpperCase();
+const shortsContainerActiveAttribute = "is-active";
+
 // Take screenshot
 captureScreenshot = function () {
   logger("Capturing screenshot");
-  var canvas = document.createElement("canvas");
-  var video = document.querySelector("video");
+
+  // Several <video> tags can be present in the document at the same time
+  // Sometimes (e.g. Youtube Shorts), the first is not the one playing the current stream
+  // Add "src" attribute filter however seems to do the trick
+  let video = document.querySelector("video[src]");
+
   if (video.mediaKeys != null) {
     browser.runtime.sendMessage({cmd: "showProtectionError"});
     return;
   }
-  var ctx = canvas.getContext("2d");
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
+
+  let canvas = document.createElement("canvas");
+  let ctx = canvas.getContext("2d");
+  canvas.width = parseInt(video.videoWidth);
+  canvas.height = parseInt(video.videoHeight);
   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
   if (copyToClipboardEnabled)
@@ -26,15 +41,13 @@ captureScreenshot = function () {
 };
 
 downloadFile = function (canvas, video) {
-  var aClass = "youtube-screenshot-a";
-  var a = document.createElement("a");
+  let a = document.createElement("a");
   a.href = canvas.toDataURL(imageFormat);
   a.download = getFileName(video);
   a.style.display = "none";
-  a.classList.add(aClass);
   document.body.appendChild(a);
-  document.querySelector(`.${aClass}`).click();
-  document.body.removeChild(a);
+  a.click();
+  a.remove();
 };
 
 function copyToClipboard(canvas) {
@@ -53,87 +66,170 @@ function copyToClipboard(canvas) {
     }, "image/png");
 }
 
-getFileName = function (video) {
-  seconds = video.currentTime;
-  mins = seconds / 60;
-  secs = seconds % 60;
-  m = mins.toString();
-  s = secs.toString();
-  mm = m.substring(0, m.indexOf("."));
-  ss = s.substring(0, s.indexOf("."));
-  if (ss.length == 1) {
-    ss = "0" + ss;
+function getFileName(video) {
+  let timeString = "";
+  const seconds = video.currentTime;
+  let mins = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds - (mins * 60));
+
+  let s = secs.toString();
+  if (s.length == 1)
+    s = "0" + s;
+
+  if (mins >= 60) {
+    const hours = Math.floor(mins / 60);
+    mins -= (hours * 60);
+
+    let m = mins.toString();
+    if (m.length == 1)
+      m = "0" + m;
+
+    timeString = `${hours}-${m}-${s}`;
+  } else {
+    timeString += `${mins}-${s}`;
   }
-  return `${window.document.title} - ${mm}:${ss}.${imageFormatExtension}`;
+
+  return `${window.document.title} - ${timeString}.${imageFormatExtension}`;
 };
 
-addButtonOnYoutubePlayer = function (controlsDiv) {
+function addButtonOnPlayer(container, regularNotShorts) {
+  const btnClass = regularNotShorts ? "ytp-screenshot" : "ytd-screenshot";
+  const type = regularNotShorts ? "regular" : "shorts";
+
   // Check if button already present
-  let previousBtn = document.querySelector("button.ytp-screenshot");
+  let previousBtn = container.querySelector(`button.${btnClass}`);
   if (previousBtn) {
-    logger("Removing previous screenshot button");
+    logger(`Removing previous ${type} screenshot button`);
     previousBtn.remove();
   }
 
-  logger("Adding screenshot button");
+  logger(`Adding ${type} screenshot button`);
   let btn = document.createElement("button");
   let t = document.createTextNode("Screenshot");
-  btn.classList.add("ytp-time-display");
-  btn.classList.add("ytp-button");
-  btn.classList.add("ytp-screenshot");
+
+  if (regularNotShorts) {
+    btn.classList.add("ytp-time-display");
+    btn.classList.add("ytp-button");
+  } else {
+    btn.classList.add("ytd-shorts-player-controls");
+    btn.style.color = getComputedStyle(container.querySelector("yt-icon")).color;
+
+    btn.style.border = "none";
+    btn.style.cursor = "pointer";
+    btn.style.background = "none";
+  }
+
+  btn.classList.add(btnClass);
   btn.style.width = "auto";
   btn.appendChild(t);
-  controlsDiv.insertBefore(btn, controlsDiv.firstChild);
 
-  logger("Adding button event listener");
+  if (regularNotShorts)
+    container.insertBefore(btn, container.firstChild);
+  else
+    container.insertBefore(btn, container.querySelector("yt-icon-button").nextSibling);
+
+  logger(`Adding ${type} button event listener`);
   btn.removeEventListener("click", captureScreenshot);
   btn.addEventListener("click", captureScreenshot);
 };
 
-function waitForYoutubeControls(callback) {
-  const controlsClass = "ytp-right-controls";
-
-  const controlsDiv = document.querySelector(`.${controlsClass}`);
-  if (controlsDiv) {
-    callback(controlsDiv);
-    return;
-  }
-
-  // Controls are not yet ready, wait for them
-  logger("Wait for Youtube controls");
+function observeShortsContainer(element, shortsCallback) {
+  logger("Observe shorts container");
 
   let observer = new MutationObserver((mutations) => {
-    mutations.forEach(mutation => {
+    for (const mutation of mutations)
+    {
+      if (mutation.attributeName != shortsContainerActiveAttribute)
+        continue;
+
+      if (element.getAttribute(shortsContainerActiveAttribute) == null) {
+        // No more the active container
+        observer.disconnect();
+
+        // Find the new active container
+        const container = document.querySelector(`${shortsContainerTag}[${shortsContainerActiveAttribute}]`);
+        if (!container) {
+          logger("No more active shorts container");
+          continue;
+        }
+
+        retrieveShortsControls(container, shortsCallback);
+        break;
+      }
+    }
+  });
+
+  observer.observe(element, { attributes: true, childList: false, subtree: false });
+}
+
+function retrieveShortsControls(container, shortsCallback) {
+  const controls = container.querySelector("ytd-shorts-player-controls");
+  if (controls) {
+    // Monitor container to catch when active one changes
+    observeShortsContainer(container, shortsCallback);
+
+    logger("Found shorts controls");
+    shortsCallback(controls);
+  }
+}
+
+function waitForControls(regularCallback, shortsCallback) {
+  logger("Wait for controls");
+
+  const regularControlsClass = "ytp-right-controls";
+
+  const regularControls = document.querySelector(`.${regularControlsClass}`);
+  if (regularControls)
+    regularCallback(regularControls);
+
+  const shortsContainer = document.querySelector(`${shortsContainerTag}[${shortsContainerActiveAttribute}]`);
+  if (shortsContainer)
+    retrieveShortsControls(shortsContainer, shortsCallback);
+
+  // Monitor controls:
+  // - wait for them to be added to document
+  // - detect when switching from regular to shorts or vice versa
+  logger("Monitor controls");
+
+  let observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
       if (!mutation.addedNodes)
         return;
 
       for (let element of mutation.addedNodes) {
-        if (element.classList.contains(controlsClass)) {
-          observer.disconnect();
-          logger("Found Youtube controls");
-          callback(element);
+
+        if (element.nodeType != Node.ELEMENT_NODE)
+          continue;
+
+        if (element.classList.contains(regularControlsClass)) {
+          logger("Found regular controls");
+          regularCallback(element);
+          continue;
+        }
+
+        if ((element.tagName === shortsContainerTagName)
+          && (element.getAttribute(shortsContainerActiveAttribute) != null)) {
+            retrieveShortsControls(element, shortsCallback);
+            break;
         }
       }
-    })
-  })
+    }
+  });
 
   observer.observe(document.body, { childList: true, subtree: true });
 }
 
-logger = function (message) {
-  if (loggingEnabled) {
-    console.log(`Youtube Screenshot Addon: ${message}`);
-  }
-};
-
-// Initialization
+// Initialization (logger is not yet really initialized for the moment)
 console.log("Initializing Youtube Screenshot Addon");
 
-var storageItem = browser.storage.local.get();
+let storageItem = browser.storage.local.get();
 storageItem.then((result) => {
   if (result.YouTubeScreenshotAddonisDebugModeOn) {
-    logger("Addon initializing!");
-    loggingEnabled = true;
+    logger = (message) => {
+        console.log(`Youtube Screenshot Addon: ${message}`);
+    };
+
+    logger("Logger enabled");
   }
 
   if (result.screenshotAction === "clipboard") {
@@ -147,7 +243,12 @@ storageItem.then((result) => {
     logger(`Setting image format to: ${imageFormat}`);
   }
 
-  waitForYoutubeControls(controlsDiv => {
-    addButtonOnYoutubePlayer(controlsDiv);
-  });
+  waitForControls(
+    (regularControls) => {
+      addButtonOnPlayer(regularControls, true);
+    },
+    (shortsControls) => {
+      addButtonOnPlayer(shortsControls, false);
+    }
+  );
 });
