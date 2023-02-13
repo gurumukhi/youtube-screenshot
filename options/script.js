2f68e971de51998f364f8e206d15f363ee0a0d5d
Jonathan GUILLOT <jonathan@joggee.fr>
2023-02-13 22:14:55 +0100

    Remove debug logs and reindent the same as others option script

--- options/script.js
+++ options/script.js
@@ -1,38 +1,34 @@
 function saveOptions(e) {
-    browser.storage.local.set({
-        YouTubeScreenshotAddonisDebugModeOn: document.querySelector('input[name=debugMode]:checked') &&
-                    document.querySelector('input[name=debugMode]:checked').value === 'debugOn',
-        screenshotAction: document.querySelector('select[name=action]').value,
-        imageFormat: document.querySelector('select[name=format]').value,
-    });
-    e.preventDefault();
+  browser.storage.local.set({
+    YouTubeScreenshotAddonisDebugModeOn: document.querySelector('input[name=debugMode]:checked') &&
+    document.querySelector('input[name=debugMode]:checked').value === 'debugOn',
+    screenshotAction: document.querySelector('select[name=action]').value,
+    imageFormat: document.querySelector('select[name=format]').value,
+  });
+  e.preventDefault();
 }
 
 function restoreOptions() {
-    document.querySelector("select[name=action]").addEventListener("change", (event) => {
-        console.log(event);
-        document.querySelector('div#format').hidden =
-            document.querySelector('option[value=clipboard]').selected;
-    });
+  document.querySelector("select[name=action]").addEventListener("change", (event) => {
+    document.querySelector('div#format').hidden =
+    document.querySelector('option[value=clipboard]').selected;
+  });
 
-    var storageItem = browser.storage.local.get();
-    storageItem.then((value) => {
-        console.log(value);
-        if (value.YouTubeScreenshotAddonisDebugModeOn) {
-            document.querySelector('input[value=debugOn]').checked = true;
-        } else {
-            document.querySelector('input[value=debugOff]').checked = true;
-        }
+  browser.storage.local.get().then((value) => {
+    if (value.YouTubeScreenshotAddonisDebugModeOn) {
+      document.querySelector('input[value=debugOn]').checked = true;
+    } else {
+      document.querySelector('input[value=debugOff]').checked = true;
+    }
 
-        if (value.screenshotAction === "clipboard") {
-            console.log(document.querySelector('#format'));
-            document.querySelector('option[value=clipboard]').selected = true;
-            document.querySelector('#format').hidden = true;
-        }
+    if (value.screenshotAction === "clipboard") {
+      document.querySelector('option[value=clipboard]').selected = true;
+      document.querySelector('#format').hidden = true;
+    }
 
-        if (value.imageFormat === "png")
-            document.querySelector('option[value=png]').selected = true;
-    });
+    if (value.imageFormat === "png")
+    document.querySelector('option[value=png]').selected = true;
+  });
 }
 
 document.querySelector('input[value=debugOff]').checked = true; // Default behaviour
