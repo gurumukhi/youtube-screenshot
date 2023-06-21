26dd01efe97e6ed661c8f27417a94a8c16ab667e
Jonathan GUILLOT <jonathan@joggee.fr>
2023-06-21 14:18:21 +0200

    Reindent options script to be coherent with content script

--- options/script.js
+++ options/script.js
@@ -4,46 +4,46 @@ const formatFieldset = document.querySelector("fieldset#format");
 const formatPngInput = document.querySelector("input[value=formatPng]");
 
 function saveOptions(e) {
-    browser.storage.local.set({
-        YouTubeScreenshotAddonisDebugModeOn: debugOnInput.checked,
-        screenshotAction: actionClipboardInput.checked ? "clipboard" : "file",
-        imageFormat: formatPngInput.checked ? "png" : "jpeg",
-    });
-    e.preventDefault();
+  browser.storage.local.set({
+    YouTubeScreenshotAddonisDebugModeOn: debugOnInput.checked,
+    screenshotAction: actionClipboardInput.checked ? "clipboard" : "file",
+    imageFormat: formatPngInput.checked ? "png" : "jpeg",
+  });
+  e.preventDefault();
 }
 
 function handleAction() {
-    if (actionClipboardInput.checked) {
-        formatFieldset.disabled = true;
-        formatPngInput.checked = true;
-    } else {
-        formatFieldset.disabled = false;
-    }
+  if (actionClipboardInput.checked) {
+    formatFieldset.disabled = true;
+    formatPngInput.checked = true;
+  } else {
+    formatFieldset.disabled = false;
+  }
 }
 
 function restoreOptions() {
-    document.querySelectorAll("fieldset#action input").forEach((input) => {
-        input.addEventListener("change", handleAction);
-    });
+  document.querySelectorAll("fieldset#action input").forEach((input) => {
+    input.addEventListener("change", handleAction);
+  });
 
-    browser.storage.local.get().then((value) => {
-        if (value.YouTubeScreenshotAddonisDebugModeOn)
-            debugOnInput.checked = true;
-        else
-            document.querySelector("input[value=debugOff]").checked = true;
+  browser.storage.local.get().then((value) => {
+    if (value.YouTubeScreenshotAddonisDebugModeOn)
+    debugOnInput.checked = true;
+    else
+    document.querySelector("input[value=debugOff]").checked = true;
 
-        if (value.screenshotAction === "clipboard")
-            actionClipboardInput.checked = true;
-        else
-            document.querySelector("input[value=actionFile]").checked = true;
+    if (value.screenshotAction === "clipboard")
+    actionClipboardInput.checked = true;
+    else
+    document.querySelector("input[value=actionFile]").checked = true;
 
-        if (value.imageFormat === "png")
-            formatPngInput.checked = true;
-        else
-            document.querySelector("input[value=formatJpeg]").checked = true;
+    if (value.imageFormat === "png")
+    formatPngInput.checked = true;
+    else
+    document.querySelector("input[value=formatJpeg]").checked = true;
 
-        handleAction();
-    });
+    handleAction();
+  });
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
