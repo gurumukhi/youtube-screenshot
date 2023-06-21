c05392c0f62f7f8948dde7b77314ca86b758457c
Jonathan GUILLOT <jonathan@joggee.fr>
2023-06-21 16:35:08 +0200

    Use radio buttons for all options (#47)
    * Simplify a bit debug mode option management
    
    * Use <fieldset> instead of <div> to group options
    
    * Use <input type="radio"> instead of <select> for all options
    
    * Reindent options script to be coherent with content script

--- options/script.js
+++ options/script.js
@@ -1,40 +1,50 @@
+const debugOnInput = document.querySelector("input[value=debugOn]");
+const actionClipboardInput = document.querySelector("input[value=actionClipboard]");
+const formatFieldset = document.querySelector("fieldset#format");
+const formatPngInput = document.querySelector("input[value=formatPng]");
+
 function saveOptions(e) {
-    browser.storage.local.set({
-        YouTubeScreenshotAddonisDebugModeOn: document.querySelector('input[name=debugMode]:checked') &&
-                    document.querySelector('input[name=debugMode]:checked').value === 'debugOn',
-        screenshotAction: document.querySelector('select[name=action]').value,
-        imageFormat: document.querySelector('select[name=format]').value,
-    });
-    e.preventDefault();
+  browser.storage.local.set({
+    YouTubeScreenshotAddonisDebugModeOn: debugOnInput.checked,
+    screenshotAction: actionClipboardInput.checked ? "clipboard" : "file",
+    imageFormat: formatPngInput.checked ? "png" : "jpeg",
+  });
+  e.preventDefault();
+}
+
+function handleAction() {
+  if (actionClipboardInput.checked) {
+    formatFieldset.disabled = true;
+    formatPngInput.checked = true;
+  } else {
+    formatFieldset.disabled = false;
+  }
 }
 
 function restoreOptions() {
-    document.querySelector("select[name=action]").addEventListener("change", (event) => {
-        console.log(event);
-        document.querySelector('div#format').hidden =
-            document.querySelector('option[value=clipboard]').selected;
-    });
+  document.querySelectorAll("fieldset#action input").forEach((input) => {
+    input.addEventListener("change", handleAction);
+  });
 
-    var storageItem = browser.storage.local.get();
-    storageItem.then((value) => {
-        console.log(value);
-        if (value.YouTubeScreenshotAddonisDebugModeOn) {
-            document.querySelector('input[value=debugOn]').checked = true;
-        } else {
-            document.querySelector('input[value=debugOff]').checked = true;
-        }
+  browser.storage.local.get().then((value) => {
+    if (value.YouTubeScreenshotAddonisDebugModeOn)
+    debugOnInput.checked = true;
+    else
+    document.querySelector("input[value=debugOff]").checked = true;
 
-        if (value.screenshotAction === "clipboard") {
-            console.log(document.querySelector('#format'));
-            document.querySelector('option[value=clipboard]').selected = true;
-            document.querySelector('#format').hidden = true;
-        }
+    if (value.screenshotAction === "clipboard")
+    actionClipboardInput.checked = true;
+    else
+    document.querySelector("input[value=actionFile]").checked = true;
 
-        if (value.imageFormat === "png")
-            document.querySelector('option[value=png]').selected = true;
-    });
+    if (value.imageFormat === "png")
+    formatPngInput.checked = true;
+    else
+    document.querySelector("input[value=formatJpeg]").checked = true;
+
+    handleAction();
+  });
 }
 
-document.querySelector('input[value=debugOff]').checked = true; // Default behaviour
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.querySelector("#save").addEventListener("click", saveOptions);
