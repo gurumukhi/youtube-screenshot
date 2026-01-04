3dfbfb1de4798c71e95db6d7daf84d5ba8620d88
Jonathan GUILLOT <jonathan@joggee.fr>
2026-01-04 14:38:57 +0100

    Remove "- YouTube" suffix from screenshot filename (#88)
    Screenshot filename is based on current page title that is now suffixed
    with "- YouTube". Simply remove it.

--- src/content_script.js
+++ src/content_script.js
@@ -68,6 +68,14 @@ function copyToClipboard(canvas) {
     }, "image/png");
 }
 
+function getTitle() {
+  let title = window.document.title;
+
+  // Remove suffix
+  const suffix = " - YouTube";
+  return title.endsWith(suffix) ? title.slice(0, -suffix.length) : title;
+}
+
 function getFileName(video) {
   let timeString = "";
   const seconds = video.currentTime;
@@ -91,8 +99,8 @@ function getFileName(video) {
     timeString += `0-`+`${mins}-${s}`;
   }
 
-  return `${window.document.title} - ${timeString}.${currentConfiguration.imageFormatExtension}`;
-};
+  return `${getTitle()} - ${timeString}.${currentConfiguration.imageFormatExtension}`;
+}
 
 function setShortsButtonStyle(btn) {
   btn.innerHTML = `
