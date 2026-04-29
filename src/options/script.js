49528aa89318051e4a2955a24735ba6e91d1ca4c
Jonathan GUILLOT <jonathan@joggee.fr>
2026-04-29 22:33:31 +0200

    Factorize addon option management

--- src/options/script.js
+++ src/options/script.js
@@ -1,14 +1,7 @@
-const debugOnInput = document.querySelector("input[value=debugOn]");
-const actionClipboardInput = document.querySelector("input[value=actionClipboard]");
-const actionBothInput = document.querySelector("input[value=actionBoth]");
-const formatFieldset = document.querySelector("fieldset#format");
-const formatPngInput = document.querySelector("input[value=formatPng]");
-const shortcutOffInput = document.querySelector("input[value=shortcutOff]");
-const saveAsOnInput = document.querySelector("input[value=saveAsOn]");
-
 function isPopup() {
   return (location.hash === '#popup');
 }
+
 // Send message to active tabs to reload configuration
 async function sendReloadToTabs() {
   const tabs = await browser.tabs.query({});
@@ -22,87 +15,92 @@ async function sendReloadToTabs() {
   }
 }
 
+const options = {
+  YouTubeScreenshotAddonisDebugModeOn: {
+    root: document.querySelector("fieldset#debug"),
+    type: "boolean",
+    default: false,
+  },
+  screenshotAction: {
+    root: document.querySelector("fieldset#action"),
+    type: "value",
+    default: "file",
+  },
+  imageFormat: {
+    root: document.querySelector("fieldset#format"),
+    type: "value",
+    default: "png",
+  },
+  shortcutEnabled: {
+    root: document.querySelector("fieldset#shortcut"),
+    type: "boolean",
+    default: true,
+  },
+  saveAsEnabled: {
+    root: document.querySelector("fieldset#saveAs"),
+    type: "boolean",
+    default: false,
+  }
+};
+
+const actionClipboardInput = options.screenshotAction.root.querySelector("input[value=clipboard]");
+const formatPngInput = options.imageFormat.root.querySelector("input[value=png]");
+
 async function saveOptions(e) {
   e.preventDefault();
 
-  // Sceenshot action
-  let screenshotAction = "file";
-  if (actionClipboardInput.checked)
-    screenshotAction = "clipboard";
-  else if (actionBothInput.checked)
-    screenshotAction = "both";
+  let selectedOptions = {};
 
-  await browser.storage.local.set({
-    YouTubeScreenshotAddonisDebugModeOn: debugOnInput.checked,
-    screenshotAction: screenshotAction,
-    imageFormat: formatPngInput.checked ? "png" : "jpeg",
-    shortcutEnabled: !shortcutOffInput.checked,
-    saveAsEnabled: saveAsOnInput.checked,
-  });
+  for (const [key, option] of Object.entries(options)) {
+    if (option.type == "boolean") {
+      const selected = option.root.querySelector("input:checked");
+      selectedOptions[key] = (selected.value == "on");
+    } else {
+      const selected = option.root.querySelector("input:checked");
+      selectedOptions[key] = selected.value;
+    }
+  }
 
+  await browser.storage.local.set(selectedOptions)
   await sendReloadToTabs();
 
-  // In case the preferences are saved from popup,
-  // just close this window
+  // In case the preferences are saved from popup, just close this window
   if (isPopup())
     window.close();
 }
 
 function handleAction() {
   if (actionClipboardInput.checked) {
-    formatFieldset.disabled = true;
+    options.imageFormat.root.disabled = true;
     formatPngInput.checked = true;
   } else {
-    formatFieldset.disabled = false;
+    options.imageFormat.root.disabled = false;
   }
 }
 
-function restoreOptions() {
+async function restoreOptions() {
   document.querySelectorAll("fieldset#action input").forEach((input) => {
     input.addEventListener("change", handleAction);
   });
 
-  browser.storage.local.get().then((value) => {
-    // Debug mode
-    if (value.YouTubeScreenshotAddonisDebugModeOn)
-      debugOnInput.checked = true;
-    else
-      document.querySelector("input[value=debugOff]").checked = true;
+  const savedOptions = await browser.storage.local.get();
 
-    // Screenshot action
-    if (value.screenshotAction === "clipboard")
-      actionClipboardInput.checked = true;
-    else if (value.screenshotAction == "both")
-      actionBothInput.checked = true;
-    else
-      document.querySelector("input[value=actionFile]").checked = true;
+  for (const [key, option] of Object.entries(options)) {
+    const value = savedOptions[key] ?? option.default;
+    if (option.type == "boolean") {
+      option.root.querySelector(`input[value=${value ? "on" : "off"}]`).checked = true;
+    } else {
+      option.root.querySelector(`input[value=${value}]`).checked = true;
+    }
+  }
 
-    // Image format
-    if (value.imageFormat === "png")
-      formatPngInput.checked = true;
-    else
-      document.querySelector("input[value=formatJpeg]").checked = true;
-
-    // Shortcut
-    if (value.shortcutEnabled === false)
-      shortcutOffInput.checked = true
-    else
-      document.querySelector("input[value=shortcutOn]").checked = true;
-
-    // Save as
-    if (value.saveAsEnabled === true)
-      saveAsOnInput.checked = true
-    else
-      document.querySelector("input[value=saveAsOff]").checked = true;
-
-    handleAction();
-  });
+  handleAction();
 }
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
+document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("#save").addEventListener("click", saveOptions);
-
-// Ensure padding for popup
-if (isPopup()) {
-  document.body.classList.add("popup");
-}
+document.querySelector("#restore").addEventListener("click", async () => {
+  await browser.storage.local.remove(Object.keys(options));
+  restoreOptions();
+  await sendReloadToTabs();
+});
